6db1c06147031147d97eda835de16d5149350414
zhuojuncheng <1094729303@qq.com>
2019-05-30 17:30:45 +0800

    类实例化增加关键字变量

--- test.py
+++ test.py
@@ -1,22 +1,22 @@
 from init import VeriCodeinit as VCinit
 
 
-VCinit = VCinit(240,60,60,240)
-#背景填充像素
-VCinit.backdrop()
+VCinit = VCinit(width_infeed=240,height_infeed=60,width_endwise=60,height_endwise=240)
+VCinit.backdrop()#背景填充像素
+
 if __name__ == '__main__':
-    # 随机生成4位验证码并显示及保存
+    # 随机生成4位验证码
     for j in range(5):
         for i in range(4):
             VCinit.draw_infeed.text((58 * i + 20, 10), VCinit.randChar(), fill=VCinit.randColor_Font(),
                                     font=VCinit.font)  # 横向显示
             VCinit.draw_endwise.text((18, 58 * i + 10), VCinit.randChar(), fill=VCinit.randColor_Font(),
                                      font=VCinit.font)  # 纵向显示
-        VCinit.img_infeed.show()
-        VCinit.img_endwise.show()
-        VCinit.img_infeed.save('E:\PycharmProjects\Verification_Code\CODE\img_infeed{}.jpg'.format(j))
-        VCinit.img_endwise.save('E:\PycharmProjects\Verification_Code\CODE\img_endwise{}.jpg'.format(j))
-        VCinit.backdrop()
+        VCinit.img_infeed.show()#横向验证码显示
+        VCinit.img_endwise.show()#纵向验证码显示
+        VCinit.img_infeed.save('E:\PycharmProjects\Verification_Code\CODE\img_infeed{}.jpg'.format(j))#保存横向验证码
+        VCinit.img_endwise.save('E:\PycharmProjects\Verification_Code\CODE\img_endwise{}.jpg'.format(j))#保存纵向验证码
+        VCinit.backdrop()#背景填充像素
     VCinit.img_infeed.close()
     VCinit.img_endwise.close()
 
